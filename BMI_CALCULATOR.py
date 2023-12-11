c4101e2e2014dd6a9b79a32a7282b6e35283c0f4
oberoivasu <oberoivasu16@gmail.com>
2023-12-12 02:16:54 +0530

    Added brief about Bmi - Calculator

--- BMI_CALCULATOR.py
+++ BMI_CALCULATOR.py
@@ -1,10 +1,20 @@
+#Write a program that interprets the Body Mass Index (BMI) based on a user's weight and height.
+
+#It should tell them the interpretation of their BMI based on the BMI value.
+
+#Under 18.5 they are underweight
+#Over 18.5 but below 25 they have a normal weight
+#Equal to or over 25 but below 30 they are slightly overweight
+#Equal to or over 30 but below 35 they are obese
+#Equal to or over 35 they are clinically obese.
+
 # Enter your height in meters e.g., 1.55
 height = float(input())
 # Enter your weight in kilograms e.g., 72
 weight = int(input())
-# 🚨 Don't change the code above 👆
 
-#Write your code below this line 👇
+# BMI calculator
+
 BMI = float(weight/(height**2))
 if(BMI<18.5):
   print(f"Your BMI is {BMI}, you are underweight.")
